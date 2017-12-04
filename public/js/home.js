075db87b1ecb78c8595c2f89004da8c92c4172fa
collier1 <collier1@purdue.edu>
2017-12-03 20:34:51 -0500

    Added an input box to home and btn for adding lists

--- public/js/home.js
+++ public/js/home.js
@@ -153,87 +153,28 @@ function add_style(i) {
     document.body.appendChild(sheet);    
 }
 
-/*function create_list_buttons() {  
-    //example of input
-    var item1 = {
-        name:"apple",
-        quantity: 2,
-        type: "fruit",
-        aquired: true
-    };
-    var item2 = {
-        name:"pear",
-        quantity: 2,
-        type: "fruit",
-        aquired: false
-    };
-    var item3 = {
-        name:"chicken",
-        quantity: 2,
-        type: "protein",
-        aquired: false
-    };
-    var item4 = {
-        name:"spinach",
-        quantity: 6,
-        type: "vegetable",
-        aquired: false
-    };
-    var item5 = {
-        name:"cheese",
-        quantity: 4,
-        type: "dairy",
-        aquired: false
-    };
-    var item6 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item7 = {
-        name:"milk",
-        quantity: 1,
-        type: "dairy",
-        aquired: false
-    };
-    var item8 = {
-        name:"orange",
-        quantity: 2,
-        type: "fruit",
-        aquired: false
-    };
-    var item9 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item10 = {
-        name:"bread",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item11 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item12 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var items1 = [item1, item2, item3];
-    var items2 = [item4, item5, item6];
-    var items3 = [item7, item8, item9];
-    var items4 = [item10, item11, item12];
-    var list1 = {list_name: "list_a", all: items1};
-    var list2 = {list_name: "list_b", all: items2};
-    var list3 = {list_name: "list_c", all: items3};
-    var list4 = {list_name: "list_d", all: items4};
-    var total = [list1, list2, list3, list4];
-} */
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
