1f0e1dcefe4591043b99d41a317ac51bcb05eeb1
tang259 <tang259@purdue.edu>
2017-12-03 19:56:55 -0500

    fixed browseRecipes

--- public/js/home.js
+++ public/js/home.js
@@ -5,87 +5,7 @@ function body_onLoad() {
 
 function create_table() {
     //example of input
-    var item1 = {
-        name:"apple",
-        quantity: 2,
-        type: "fruit",
-        aquired: true
-    };
-    var item2 = {
-        name:"pear",
-        quantity: 2,
-        type: "fruit",
-        aquired: false
-    };
-    var item3 = {
-        name:"chicken",
-        quantity: 2,
-        type: "protein",
-        aquired: false
-    };
-    var item4 = {
-        name:"spinach",
-        quantity: 6,
-        type: "vegetable",
-        aquired: false
-    };
-    var item5 = {
-        name:"cheese",
-        quantity: 4,
-        type: "dairy",
-        aquired: false
-    };
-    var item6 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item7 = {
-        name:"milk",
-        quantity: 1,
-        type: "dairy",
-        aquired: false
-    };
-    var item8 = {
-        name:"orange",
-        quantity: 2,
-        type: "fruit",
-        aquired: false
-    };
-    var item9 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item10 = {
-        name:"bread",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item11 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var item12 = {
-        name:"cereal",
-        quantity: 2,
-        type: "grain",
-        aquired: false
-    };
-    var items1 = [item1, item2, item3];
-    var items2 = [item4, item5, item6];
-    var items3 = [item7, item8, item9];
-    var items4 = [item10, item11, item12];
-    var list1 = {list_name: "list_a", all: items1};
-    var list2 = {list_name: "list_b", all: items2};
-    var list3 = {list_name: "list_c", all: items3};
-    var list4 = {list_name: "list_d", all: items4};
-    var total = [list1, list2, list3, list4];
+
     var table = '';
     var list = total[1];
     var rows = list.all.length + 1;
@@ -189,58 +109,6 @@ function getList(listName) {
 				return;
 		});
 }
-
-function getListNotAcquired(listName) {
-	var props = {
-        //not 100% sure this is the correct way for database
-		method: 'GET ListOfItems Where acquired = False'
-	};
-
-	fetch(BASE_URL + `/lists/get?listName={listName}`, props)
-		.then(function(list) {
-			if (list.ok) {
-				return list;
-			}
-			else {
-				console.log("couldn't get the list ");
-
-				//TODO couldn't get the list for some reason, display an error message
-
-				return;
-			}
-		}).catch(function(err) {
-				console.log("there was a network error");
-				console.log(err);
-				return;
-		});
-}
-
-function getListAquired(listName) {
-	var props = {
-		method: 'GET ListOfItems Where acquired = True'
-	};
-
-	fetch(BASE_URL + `/lists/get?listName={listName}`, props)
-		.then(function(list) {
-			if (list.ok) {
-				return list;
-			}
-			else {
-				console.log("couldn't get the list ");
-
-				//TODO couldn't get the list for some reason, display an error message
-
-				return;
-			}
-		}).catch(function(err) {
-				console.log("there was a network error");
-				console.log(err);
-				return;
-		});
-}
-
-
-
 
 function getUserLists() {
 	//get the username and password that were saved to local storage when the user loged in
