5f2ed077a2211e527447992111822305d3a2b7b3
collier1 <collier1@purdue.edu>
2017-12-03 20:48:56 -0500

    Made home able to add new lists

--- public/js/home.js
+++ public/js/home.js
@@ -154,27 +154,9 @@ function add_style(i) {
 }
 
 function newElement() {
-    var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
+    //get users list and also make sure that this list doesn't already exist
+    if(inputValue != "") {
+        create_button(inputValue);
     }
   }
